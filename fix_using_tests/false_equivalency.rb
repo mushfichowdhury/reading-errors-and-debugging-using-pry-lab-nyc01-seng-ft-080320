1c255763c7280b5e821804551a1ee8666ece9e4f
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-07-29 06:43:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
@@ -10,7 +10,8 @@ def prompt_user
   puts "2.) Eat a ham."
 end
 
-def selection(num)
+def selection(get_user_input)
+  num = get_user_input
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
